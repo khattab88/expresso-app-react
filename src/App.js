ba369d8f33e90b92c17d4856dfa1ff70b7b05dbf
khattab88 <m_khatab_88@hotmail.com>
2021-02-17 10:51:49 +0200

    clean project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello Expresso App</h1>
-      </header>
     </div>
   );
 }
