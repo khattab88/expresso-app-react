e8ffc5278d304d2afdec6059997c3f487e512172
khattab88 <m_khatab_88@hotmail.com>
2021-03-22 06:21:48 +0200

    use router link in footer

--- src/components/footer/secondary-nav-item/SecondaryNavItem.js
+++ src/components/footer/secondary-nav-item/SecondaryNavItem.js
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 function SecondaryNavItem(props) {
     return(
         <li className="secondary-navigation__item">
-            <a href={props.href} className="secondary-navigation__link">{props.title}</a>
+            <Link to={props.href} className="secondary-navigation__link">{props.title}</Link>
         </li>
     );
 }
