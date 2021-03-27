31ae96f46e6c2c6844d127f8fe55ad002eba9455
khattab88 <m_khatab_88@hotmail.com>
2021-03-27 22:24:13 +0200

    fix restaurant showcase card links

--- src/components/restaurants-showcase/restaurant-showcase-card/RestaurantShowcaseCard.js
+++ src/components/restaurants-showcase/restaurant-showcase-card/RestaurantShowcaseCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class RestaurantShowcaseCard extends React.Component {
     constructor(props) {
@@ -12,11 +13,11 @@ class RestaurantShowcaseCard extends React.Component {
 
         return (
             <article className={className} id={this.props.restaurantCard.id}>
-                <a className="restaurant-card__link" href={`/restaurant-menu/${this.props.restaurantCard.id}`}>
+                <Link className="restaurant-card__link" to={`/restaurant-menu/${this.props.restaurantCard.id}`}>
                     <img className="restaurant-card__img" src={this.props.restaurantCard.image} alt={this.props.restaurantCard.name} />
                     <div className="restaurant-card__name">{this.props.restaurantCard.name}</div>
                     <div className="restaurant-card__desc">{this.props.restaurantCard.desc}</div>
-                </a>
+                </Link>
             </article>
         );
     }
