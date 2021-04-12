7e23a67e9bc8dde89f0501c125e13438f12976b1
khattab88 <m_khatab_88@hotmail.com>
2021-04-12 20:43:11 +0200

    scroll to top when open address modal

--- src/components/profile/delivery-info/DeliveryInfo.js
+++ src/components/profile/delivery-info/DeliveryInfo.js
@@ -9,11 +9,11 @@ const DeliveryInfo = props => {
     const [isAddressModalOpen, setIsAddressModalOpen] = useState(false);
 
     const toggleAddressModal = () => {
-        // window.scroll({
-        //     top: 0,
-        //     left: 0,
-        //     behavior: 'smooth'
-        // });
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
 
         setIsAddressModalOpen(!isAddressModalOpen);
     }
