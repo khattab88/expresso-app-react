3d3a83e0394a77ad407ff21d3dbacb75807ffb20
khattab88 <m_khatab_88@hotmail.com>
2021-04-21 22:16:27 +0200

    display user orders (orders page)

--- src/components/orders/Orders.js
+++ src/components/orders/Orders.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import orderApi from '../../api/OrderApi';
 
 const Orders = (props) => {
 
     const [activeTab, setActiveTab] = useState("0");
+    const [orders, setOrders] = useState([]);
+
+    useEffect(async () => {
+        const orders = (await orderApi.getUserOrders(props.auth.user.id)).data;
+        console.log(orders);
+
+        setOrders(orders);
+    }, [])
 
     const onTabClick = e => {
         e.preventDefault();
@@ -32,32 +43,67 @@ const Orders = (props) => {
 
     const renderCurrentOrderPane = () => {
 
-        /// if(user.orders.length === 0)
-        return renderNoOrders();
+        if (orders.length === 0)
+            return renderNoOrders();
 
-        return(
-            <h3>CURRENT ORDER</h3>
-        );
+        return renderOrderList();
     }
 
     const renderPastOrdersPane = () => {
 
-        /// if(user.orders.length === 0)
-        return renderNoOrders();
+        if (orders.length === 0)
+            return renderNoOrders();
 
-        return(
-            <h3>PAST ORDERS</h3>
-        );
+        return renderOrderList();
     }
 
     const renderNoOrders = () => {
-        return(
+        return (
             <div className="no-orders">
                 <h3 className="no-orders__primary-title">No Orders found</h3>
                 <p className="no-orders__secondary-title">Ordrer through the fastest delivery network,
                      get your order delivered faster.</p>
             </div>
         );
+    }
+
+    const renderOrderList = () => {
+        const orderList = orders.map(order => {
+            return (
+                <article className="order" data-id={order.id} key={order.id}>
+                    <div className="order__summary">
+                        <img className="order__restaurant-logo" src={order.branch.restaurant.logo} alt={order.branch.restaurant.name} /> 
+                        <div className="order__details">
+                            <h4 className="order__restaurant-name">{order.branch.restaurant.name}</h4>
+                            <p className="order__date">
+                                {new Date(order.createdAt).toLocaleString("en-US", { dateStyle: "long", timeStyle: "short" })}
+                            </p>
+                            <div className="order__info">
+                                <p className="order__id">Order Id: {order._id.slice(0,10)}</p>
+                                <p className="order__status">Delivered</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="order__actions">
+                        <button className="order__btn">
+                            <span className="order__btn__icon order__btn__icon--rate"></span>
+                            <span>Rate</span>
+                        </button>
+                        <button className="order__btn">
+                            <span className="order__btn__icon order__btn__icon--view"></span>
+                            <span>View</span>
+                        </button>
+                    </div>
+                </article>
+            );
+        });
+
+        return (
+            <div className="order-list">
+                {orderList}
+            </div>
+        );
+
     }
 
     return (
@@ -80,4 +126,9 @@ const Orders = (props) => {
     );
 }
 
-export default Orders;
+
+const mapStateToProps = state => {
+    return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Orders);
